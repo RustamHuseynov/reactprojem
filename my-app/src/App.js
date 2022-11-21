501bd7b5bb4e13a3a3219ebd8cc6f9fb096bc6d4
RustamHuseynov <rustem.huseynov.2015@bk.ru>
2022-11-21 21:51:07 +0400

    ilk React Projesi

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from "./logo.svg"
+
+
+function App() {
+  return (
+    <div className="App">
+      <img src='/logo192.png'></img>
+      <img src={logo}></img>
+    </div>
+  );
+}
+
+export default App;
